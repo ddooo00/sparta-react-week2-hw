7fa655bc7eed0ae40a4e05bd13ea2df52198491f
ddooo00 <1995thdud@naver.com>
2023-06-23 13:40:16 +0900

    화면에 보여지기까지

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useSelector } from "react-redux";
 
 function List() {
+  const todos = useSelector((state) => {
+    return state.todo.todos;
+  });
+  console.log(todos);
+
   return (
     <StBox>
-      <Link to="/">돌아가기</Link>
+      {todos.map((todo) => (
+        <div>
+          <h3>{todo.title}</h3>
+          <p>{todo.body}</p>
+        </div>
+      ))}
     </StBox>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/Header";
 import Layout from "../components/Layout";
 import Search from "../components/Search";
+import List from "../components/List";
 
 const Home = () => {
   return (
     <Layout>
       <Header />
       <Search />
+      <List />
     </Layout>
   );
 };
