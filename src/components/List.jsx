69a8a0f7042ba4cdb595943ea08226c36db17ef4
ddooo00 <1995thdud@naver.com>
2023-06-23 16:50:27 +0900

    리스트부분 스타일컴포넌트로 변경

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -11,49 +11,47 @@ function List() {
   console.log(todos);
 
   return (
-    <StBox>
+    <StOuterBox>
       <h2>Working...🔥</h2>
-      <div style={{ gap: "12px", display: "flex", flexWrap: "wrap" }}>
+      <StInBox>
         {todos.map((todo) => (
-          <div
-            style={{
-              marginBottom: "10px",
-              width: "250px",
-              height: "160px",
-              border: "4px solid teal",
-              borderRadius: "12px",
-              padding: "12px 24px 24px 24px",
-            }}
-          >
+          <StListBox>
             <h3>{todo.title}</h3>
             <p>{todo.body}</p>
-          </div>
+          </StListBox>
         ))}
-      </div>
+      </StInBox>
+
       <h2>Done...🎉</h2>
-      <div style={{ gap: "12px", display: "flex", flexWrap: "wrap" }}>
+      <StInBox>
         {todos.map((todo) => (
-          <div
-            style={{
-              marginBottom: "10px",
-              width: "250px",
-              height: "160px",
-              border: "4px solid teal",
-              borderRadius: "12px",
-              padding: "12px 24px 24px 24px",
-            }}
-          >
+          <StListBox>
             <h3>{todo.title}</h3>
             <p>{todo.body}</p>
-          </div>
+          </StListBox>
         ))}
-      </div>
-    </StBox>
+      </StInBox>
+    </StOuterBox>
   );
 }
 
 export default List;
 
-const StBox = styled.div`
+const StOuterBox = styled.div`
   margin-top: 20px;
+  // background-color: yellow;
+`;
+const StInBox = styled.div`
+  gap: 12px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const StListBox = styled.div`
+  margin-bottom: 10px;
+  width: 250px;
+  height: 160px;
+  border: 4px solid teal;
+  border-radius: 12px;
+  padding: 12px 24px 24px 24px;
 `;
